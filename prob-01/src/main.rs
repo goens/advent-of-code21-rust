9e85fb2b893f4ff21bce8924923e22fe36e11da7
Andres Goens <andres.goens@ed.ac.uk>
2021-12-01 23:19:52 +0000

    second star

--- prob-01/src/main.rs
+++ prob-01/src/main.rs
@@ -9,6 +9,23 @@ fn parse(i : String) -> Vec<i32> {
     res
 }
 
+
+const window_size: usize = 3; // = 1 for first star
+
+fn sliding_window_sum(i : Vec<i32>) -> Vec<i32> {
+  let mut window : [i32; window_size] = [0; window_size];
+  let mut pos : usize = 0;
+  let mut res : Vec<i32> = Vec::new();
+  let mut window_full = false;
+    for x in i {
+        window[pos] = x;
+        pos = (pos + 1) % window_size;
+        if pos == 0 {  window_full = true }
+        if window_full { res.push(window.iter().sum())}
+    }
+    res
+}
+
 fn main() {
     let filename = "input";
     println!("In file {}", filename);
@@ -16,7 +33,8 @@ fn main() {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    let vec = parse(contents);
+    let vec_raw = parse(contents);
+    let vec = sliding_window_sum(vec_raw);
     let mut last = vec[0];
     let mut total = 0;
     for x in vec {
